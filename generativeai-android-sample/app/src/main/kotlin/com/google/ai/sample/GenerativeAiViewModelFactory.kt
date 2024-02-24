23f8b4f0722e2144b9d9a0564ed788ed562aa29b
Harsh Trivedi <hktrivedi264@gmail.com>
2024-02-24 19:13:01 +0530

    - Changed ViewModel Class Name In GenerativeViewModelFactory.kt file.

--- generativeai-android-sample/app/src/main/kotlin/com/google/ai/sample/GenerativeAiViewModelFactory.kt
+++ generativeai-android-sample/app/src/main/kotlin/com/google/ai/sample/GenerativeAiViewModelFactory.kt
@@ -24,16 +24,17 @@ import com.google.ai.sample.feature.chat.ChatViewModel
 import com.google.ai.sample.feature.multimodal.PhotoReasoningViewModel
 import com.google.ai.sample.feature.text.SummarizeViewModel
 
+@Suppress("UNCHECKED_CAST")
 val GenerativeViewModelFactory = object : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(
-        viewModelClass: Class<T>,
+        modelClass: Class<T>,
         extras: CreationExtras
     ): T {
         val config = generationConfig {
             temperature = 0.7f
         }
 
-        return with(viewModelClass) {
+        return with(modelClass) {
             when {
                 isAssignableFrom(SummarizeViewModel::class.java) -> {
                     // Initialize a GenerativeModel with the `gemini-pro` AI model
@@ -68,7 +69,7 @@ val GenerativeViewModelFactory = object : ViewModelProvider.Factory {
                 }
 
                 else ->
-                    throw IllegalArgumentException("Unknown ViewModel class: ${viewModelClass.name}")
+                    throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
             }
         } as T
     }
